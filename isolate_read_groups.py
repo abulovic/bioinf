3070bc91e826e8646063b5e21791bfeb1af50c16
abulovic <bulovic.ana@gmail.com>
2014-06-05 13:21:34 +0200

    isolate_read_groups: Added nicer plots and entropy calculation.

--- isolate_read_groups.py
+++ isolate_read_groups.py
@@ -9,12 +9,16 @@
 ###############################################################
 
 import sys
+import math
 from collections import defaultdict
 
 import pylab as plab
 from numpy import NaN, Inf, arange, isscalar, asarray, array
 from scipy.signal import argrelextrema
 import pysam
+
+WINDOW_SIZE = 100
+MIN_ALNS = 50
 
 def peakdet(v, delta, x = None):
 	maxtab = []
@@ -57,7 +61,7 @@ def peakdet(v, delta, x = None):
 
 
 
-def process_read(read, pos_distr, interesting_len=100):
+def process_read(read, pos_distr, interesting_len=WINDOW_SIZE):
 	start = read.aend - read.alen
 	end = read.aend
 	for i in xrange(start, end - interesting_len):
@@ -67,22 +71,45 @@ def process_read(read, pos_distr, interesting_len=100):
 		else:
 			pos_distr[i][subseq] += 1
 
-def plot_multimodal_read_distr(multimodal):
+def get_signal(multimodal):
 	x = range(0, max(multimodal.keys()) + 1)
 	y = array([0.] * len(x))
 	for key, val in multimodal.items():
 		y[key] = val
-	maxima, gl = peakdet(y, 0.3)
+	return x, y
+
+def get_local_maxima(sig):
+	maxima, gl = peakdet(sig, 0.3)
 	lmx = []
 	lmy = []
 	for s in maxima:
 		lmx.append(int(s[0]))
 		lmy.append(s[1])
+	return lmx, lmy
+
+def calc_entropy(probs):
+	return - sum(map(lambda p: p * math.log(p, 2), probs))
+
+def get_entropy(probs_distribution, scaled=False):
+	entropy = [0] * (max(probs_distribution.keys()) + 1)
+	for loc, distr in probs_distribution.iteritems():
+		total_reads = sum(distr.values())
+		probs = map(lambda n: float(n) / total_reads, distr.values())
+		entropy[loc] = calc_entropy(probs)
+		if scaled:
+			entropy[loc] *= total_reads
+	max_entropy = max(entropy)
+	entropy = map(lambda e: e / max_entropy, entropy)
+	return entropy
+
+
+def plot_multimodal_read_distr(x, y, lmx, lmy):
+	plab.figure()
+	plab.title('Genome locations with 2-modal read distributions, window length = %d' % WINDOW_SIZE)
 	plab.plot(x, y)
 	plab.hold(True)
 	plab.plot(lmx, lmy, 'ro')
 	plab.axis([0, len(y), 0, 2])
-	plab.show()
 
 
 def main():
@@ -111,11 +138,27 @@ def main():
 	multimodal = {}
 	for pos, covs_dict in pos_distribution.iteritems():
 		covs = sorted(covs_dict.values(), reverse=True)
-		if len(covs) > 1 and float(covs[1]) / covs[0] > 0.2:
+		if len(covs) > 1 and float(covs[1]) / covs[0] > 0.2 and sum(covs) > MIN_ALNS:
 			ambiguous += 1
 			multimodal[pos] = float(covs[1]) / covs[0]
 
-	plot_multimodal_read_distr(multimodal)
+	x, y = get_signal(multimodal)
+	lmx, lmy = get_local_maxima(y)
+	plot_multimodal_read_distr(x, y, lmx, lmy)
+
+	# We calculate the scaled entropy. 
+	# The higher the entropy, the more versatile the reads aligned
+	# to that location. 
+	# We scale the entropy with the number of alignments to that position,
+	# since we also want it to indicate whether there were many or few
+	# alignments involved.
+	entropy = get_entropy(pos_distribution, scaled=True)
+	plab.plot(entropy, 'g')
+
+	for loc in lmx:
+		print "LOC %4d: ENTROPY: %1.4f, TOTAL READS: %d, SEC/FIRST: %1.4f" % (loc, entropy[loc], sum(pos_distribution[loc].values()), multimodal[loc])
+
+	plab.show()
 
 if __name__ == '__main__':
 	main()
